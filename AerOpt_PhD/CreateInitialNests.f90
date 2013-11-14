9d6a6db731972f799ab4dd671685e000474035f2
David Naumann <717761@swansea.ac.uk>
2013-11-14 10:00:22 +0000

    + Included Displacement Matrices + Started Subroutine LHC: Create Displacement Matrix + Implement 3rd Dimension + Start Subsection Initial Nests creation

--- AerOpt_PhD/CreateInitialNests.f90
+++ AerOpt_PhD/CreateInitialNests.f90
@@ -1,8 +1,8 @@
 module CreateInitialNests
     
-        real rn                ! Counts random numbers
-        integer*4 timeArray(3)    ! Holds the hour, minute, and second
-        integer, dimension(:), allocatable :: put, get
+        real :: rn                                          ! Counts random numbers
+        real, dimension(:,:), allocatable :: MxDisp           ! Matrix with min/max Displacements
+        real, dimension(:,:), allocatable :: MxDisp_NonMov    ! Matrix with min/max Displacements
 
         !In order to get a different sequence each time, we initialize the
         !seed of the random number function with the sum of the current
@@ -14,8 +14,29 @@ module CreateInitialNests
  
         call RANDOM_SEED
         call RANDOM_NUMBER (rn)
-        print *, rn
         
-    end subroutine CreateRandomNumber 
+    end subroutine CreateRandomNumber
+    
+    subroutine LHC(NoDim, NoCtrPts, xmax, ymax, zmax)
+    
+        ! Variables
+        integer, dimension(NoCtrPts) :: ones
+        allocate(MxDisp((NoDim*2),NoCtrPts))
+          
+        ! Body of LHC       
+        do i = 1, NoCtrPts           
+            ones(i) = 1
+        end do
+! Look into Details of Row and Column Definition
+        MxDisp(1,:) = xmax*ones
+        MxDisp(2,:) = xmax*(-1)*ones
+        MxDisp(3,:) = ymax*ones
+        MxDisp(4,:) = ymax*(-1)*ones
+        if (NoDim == 3) then  
+            MxDisp(5,:) = zmax*ones
+            MxDisp(6,:) = zmax*ones
+        end if
+        
+    end subroutine LHC
     
 end module CreateInitialNests
--- AerOpt_PhD/Main.f90
+++ AerOpt_PhD/Main.f90
@@ -2,8 +2,8 @@ program AerOpt
    
     ! Initializing Parameters and Implement Modules
     use CreateInitialNests
-    integer :: NoNests, NoIter
-    real :: xmax, ymax, Ma, engFMF
+    integer :: i, j, k, NoNests, NoIter, NoCtrPts, NoDim   ! Number of Nests, Iterations, Control Points, Dimensions
+    real :: xmax, ymax, Ma, engFMF                  ! max Displacements, Mach number, ??
     character (len=4) filename
     character :: runOnCluster
     
@@ -16,14 +16,26 @@ program AerOpt
     Ma = 0.5            ! Mach Number
     xmax = 0.00         ! Maximum horizontal displacement of Control Nodes
     ymax = 0.02         ! Maximum vertical displacement of Control Nodes
+    zmax = 0.00         ! Maximum lateral displacement of Control Nodes
     NoNests = 30        ! Number of Nests (Cuckoo Search)
     NoIter = -3         ! ?? Number of Iterations
+    NoCtrPts = 7        ! Number of Control Points
+    NoDim = 3           ! Number of Dimensions
     filename = 'case'
     runOnCluster = 'Y'
-    engFMF = 1.0        ! Application?
+    engFMF = 1.0        ! What is it?
     print *, Ma
     
     !! Plot initial Mesh: Actual and Coarse Mesh! --> MATLAB output file?
-    call CreateRandomNumber
     
+    !****Sub-Section: Create Initial Nests for the CFD Solver****** 
+    !***********included in CreateInitialNests module**************
+    call CreateRandomNumber
+    print *, rn !Output: rn = Random Number
+    
+    call LHC(NoDim, NoCtrPts, xmax, ymax, zmax)                !Sampling of initial points/nests via LHC
+    print*, MxDisp
+    print*, transpose(MxDisp)
+    
+    ! Output:
 end program AerOpt
