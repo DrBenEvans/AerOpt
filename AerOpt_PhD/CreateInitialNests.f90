66e2533fcf6a45ac1e4578c5e506a67b96f4c81a
David Naumann <717761@swansea.ac.uk>
2013-11-18 08:30:32 +0000

    + Changed Allocation of MxDisp_NonMov + Include LHS Subroutine + Define Parameters as Constants

--- AerOpt_PhD/CreateInitialNests.f90
+++ AerOpt_PhD/CreateInitialNests.f90
@@ -4,26 +4,20 @@ module CreateInitialNests
         real, dimension(:,:), allocatable :: MxDisp           ! Matrix with min/max Displacements
         real, dimension(:,:), allocatable :: MxDisp_NonMov    ! Matrix with min/max Displacements
         integer, dimension(:), allocatable :: cond            ! Identifies zero/non-zero values
-
+        integer :: av                                         ! Allocater Variable
+        
     contains
     
-    subroutine CreateRandomNumber
- 
-        call RANDOM_SEED    ! Automatically generates a random initial number based on time and date
-        call RANDOM_NUMBER (rn)
-        
-    end subroutine CreateRandomNumber
-    
-    subroutine LHC(NoDim, NoCtrPts, xmax, ymax, zmax)
+    subroutine SubCreateInitialNests(NoNests, NoDim, NoCP, xmax, ymax, zmax)
     
         ! Variables
-        integer :: i, j
-        integer, dimension(NoCtrPts) :: ones
-        allocate(MxDisp((NoCtrPts*NoDim),2))
-        allocate(cond(NoCtrPts*NoDim))
+        integer, dimension(NoCP) :: ones
+        allocate(MxDisp((NoCP*NoDim),2))
+        allocate(cond(NoCP*NoDim))
+
         
-        !!****Body of LHC****!!      
-        do i = 1, NoCtrPts           
+        !!****Body of SubCreateInitialNests****!!      
+        do i = 1, NoCP           
             ones(i) = 1
         end do
         
@@ -35,18 +29,23 @@ module CreateInitialNests
         write(*,*), 'Full Matrix'
         print *, MxDisp
         
-        !! Reduction of MxDisp to Nonzero Values - (only for LHS-routine to reduce processing time)
+        !**Reduction of MxDisp to Nonzero Values - (only for LHS-routine to reduce processing time)**!
         cond = MxDisp(:,1) /= MxDisp(:,2)
+        write(*,*), 'Zero/Nonzero'
         print *, cond
         
         ! Derive size for reduced Matrix
-        j = 0
-        do i = 1, size(cond)            
-            if (cond(i) == -1) then               
-                j = j + 1
-            end if
-        end do
-        allocate(MxDisp_NonMov(j,2))
+        av = 0
+        if (xmax /= 0.00) then
+            av = av + 1
+        end if
+        if (ymax /= 0.00) then
+            av = av + 1
+        end if
+        if (zmax /= 0.00 .and. NoDim == 3) then
+            av = av + 1
+        end if
+        allocate(MxDisp_NonMov(av*NoCP,2))
         
         ! Reduce Matrix
         ! MxDisp_NonMov = MxDisp(cond,:)        
@@ -57,11 +56,117 @@ module CreateInitialNests
                 j = j + 1
             end if
         end do
-                
+        !**END Reduction of...**!
+        
         write(*,*), 'Reduced Matrix'
         print *, MxDisp_NonMov        
 
+        ! Execute Latin Hypercube Sampling with min/max Displacements
+        call LHS(MxDisp_NonMov, NoNests, NoCP)       
+        ! Output:
+        
         ! Remember to empty MxDisp_NonMov to safe storage
-    end subroutine LHC
+    end subroutine SubCreateInitialNests
+    
+    subroutine LHS(MxDisp_NonMov, NoNests, NoCP)
+    
+        ! Variables
+        real, dimension(NoCP*av,2) :: MxDisp_NonMov
+        double precision, dimension(NoNests) :: linSamp, linLoop
+        real, dimension(NoNests, NoCP) :: FinalSampPoints
+        integer :: ms
+        integer, dimension(NoNests) :: rp
+        double precision :: max, min, first, last, dx, dlS, ds, dlL_test, dlL1, dlL2, dL, a
+    
+        ! Body of LHS
+        ms = size(MxDisp_NonMov,1)
+
+        if ( ms == NoCP) then
+            
+            ! Implement Section as Sub and Implement it in other Cases 2 or 3 times respectively
+            max = MxDisp_NonMov(1,1)
+            min = MxDisp_NonMov(1,2)
+            dx = max - min
+            do i = 0, (NoNests - 1)
+                a = i/ real(NoNests - 1)
+                linSamp(i+1) = max - dx*a              
+            end do
+            dlS = linSamp(1) - linSamp(2)
+            ds = dlS/100000
+            do i = 1, 100000
+                
+                first = max - ds*i
+                last = min + ds*i
+                do j = 0, (NoNests - 1)
+                    a = j/ real(NoNests - 1)  
+                    linLoop(j+1) = first - (first - last)*a
+                end do
+                dlL1 = max - linLoop(1)
+                dlL2 = (linLoop(3) - linLoop(2))/2
+                dlL_test = (linLoop(2) - linLoop(1))/2
+                if ((dlL1 + dlL2) > 10**(-6)) then
+                    dL = max - linLoop(1)
+                    exit
+                end if
+                
+            end do
+            
+            first = max - dL
+            last = min + dL
+            do i = 0, (NoNests - 1)
+                a = i/ real(NoNests - 1)  
+                linLoop(i+1) = first - (first - last)*a
+            end do
+            
+            print *, size(linLoop)
+            print *, NoNests
+            
+            do i = 1, NoCP               
+                call randperm(NoNests, rp)
+                do j = 1, NoNests                   
+                    FinalSampPoints(j,i) = linLoop(rp(j))
+                end do
+                
+            end do
+            
+            print *, 'Case 1'
+            print *, FinalSampPoints
+            
+        elseif (ms == NoCP*2) then
+            print *, 'Case 2'
+        elseif (ms == NoCP*3) then
+            print *, 'Case 3'
+        else
+            print *, 'Impossible Number of Dimensions.'
+            stop
+        end if
+    
+    end subroutine LHS
+    
+    subroutine randperm(N, p)
+    
+!! Source: coding.derkeiler.com/Archive/Fortran/comp.lang.fortran/2006-03/msg00748.html
+!! Based on Knuth's algorithm
+
+        integer, intent(in) :: N
+        integer, dimension(:), intent(out) :: p
+
+        integer :: temp
+
+        p = (/ (i, i=1,N) /)
+
+        do j=N,2,-1
+
+            call random_number(rn)
+            k = floor(j*rn) + 1
+
+            ! exchange p(k) and p(j)
+            temp = p(k)
+            p(k) = p(j)
+            p(j) = temp
+
+        end do
+
+        end subroutine randperm
     
 end module CreateInitialNests
--- AerOpt_PhD/Main.f90
+++ AerOpt_PhD/Main.f90
@@ -2,10 +2,7 @@ program AerOpt
    
     ! Initializing Parameters and Implement Modules
     use CreateInitialNests
-    integer :: i, j, k, NoNests, NoIter, NoCtrPts, NoDim   ! Number of Nests, Iterations, Control Points, Dimensions
-    real :: xmax, ymax, Ma, engFMF                  ! max Displacements, Mach number, ??
-    character (len=4) filename
-    character :: runOnCluster
+    integer :: i, j, k                ! Simple Loop Variables
     
     ! User Input
 !!! TO-DO: create text files including the input data of meshmat & datamat
@@ -13,26 +10,28 @@ program AerOpt
     !pathexec = '/eng/cvcluster/egevansbj/codes/prepro/2Dprepro_duct'
     !pathexec2 = '/eng/cvcluster/egmanon/Duct2d'
     
-    Ma = 0.5            ! Mach Number
-    xmax = 0.00         ! Maximum horizontal displacement of Control Nodes
-    ymax = 0.02         ! Maximum vertical displacement of Control Nodes
-    zmax = 0.05         ! Maximum lateral displacement of Control Nodes
-    NoNests = 30        ! Number of Nests (Cuckoo Search)
-    NoIter = -3         ! ?? Number of Iterations
-    NoCtrPts = 7        ! Number of Control Points
-    NoDim = 2           ! Number of Dimensions
-    filename = 'case'
-    runOnCluster = 'Y'
-    engFMF = 1.0        ! What is it?
+    real, parameter :: Ma = 0.5            ! Mach Number
+    real, parameter :: xmax = 0.00         ! Maximum horizontal displacement of Control Nodes
+    real, parameter :: ymax = 0.02         ! Maximum vertical displacement of Control Nodes
+    real, parameter :: zmax = 0.05         ! Maximum lateral displacement of Control Nodes
+    real, parameter :: engFMF = 1.0        ! What is it?
+    integer, parameter :: NoNests = 30     ! Number of Nests (Cuckoo Search)
+    integer, parameter :: NoIter = -3      ! ?? Number of Iterations
+    integer, parameter :: NoCP = 7         ! Number of Control Points
+    integer, parameter :: NoDim = 2        ! Number of Dimensions
+    character (len=4), parameter :: filename = 'case'   ! filename of Meshfiles 
+    character, parameter :: runOnCluster = 'Y'
+
     
     !! PLOT initial Mesh: Actual and Coarse Mesh! --> MATLAB output file?
     
     !****Sub-Section: Create Initial Nests for the CFD Solver****** 
     !***********included in CreateInitialNests module**************
-    call CreateRandomNumber
-    print *, rn !Output: rn = Random Number
     
-    call LHC(NoDim, NoCtrPts, xmax, ymax, zmax)                !Sampling of initial points/nests via LHC
+    call RANDOM_SEED    ! Automatically generates a random initial number based on time and date
+    !call RANDOM_NUMBER (rn)
     
+    call SubCreateInitialNests(NoNests, NoDim, NoCP, xmax, ymax, zmax)                !Sampling of initial points/nests via LHC    
     ! Output:
+    
 end program AerOpt
