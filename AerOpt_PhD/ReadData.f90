d3e292751ac00fa05a749b7d9923be0115006100
David Naumann <717761@swansea.ac.uk>
2014-02-21 12:29:21 +0000

    + Main: Centralize Name Structure (In/Out Folder, default Path, version) + 	Replacement of isLin by SystemType - B, Q, W selection over just Linux or Windows +	Implementation of new System Option (Bsub queueing System of HPCWales machine)
    + ReadData:		Implementation of generalized variables
    		Bsub System integrated
    		Required: new batchfile, modified SolverInputFile, changed Access Permission

--- AerOpt_PhD/ReadData.f90
+++ AerOpt_PhD/ReadData.f90
@@ -10,15 +10,16 @@ module ReadData
     
 contains
       
-    subroutine SubReadData(NoCP, NoDim)
+    subroutine SubReadData(NoCP, NoDim, InFolder)
     ! Objective: Reads the Mesh data and Control Points Coordinates
     
     ! Variables
     implicit none
     integer :: NoDim, i, NoCP
+    character(len=*) :: InFolder
     
     ! Body of ReadData
-    open(1, file='Input_Data/Mesh_fine.txt')
+    open(1, file= InFolder//'/Mesh_fine.txt')
     read(1, 11) ne
     allocate(connecf(ne,NoDim+1))
     read(1, 11) np
@@ -38,20 +39,20 @@ contains
     close(1)
     
     allocate(Coord_CP(NoCP,NoDim))
-    open(2, file='Input_Data/Control_Nodes.txt')
+    open(2, file= InFolder//'/Control_Nodes.txt')
     do i = 1, NoCP
         read(2, *) Coord_CP(i,:)
     end do
     close(2)
     
     allocate(Rect(NoCP,NoDim*4))
-    open(3, file='Input_Data/Rectangles.txt')
+    open(3, file= InFolder//'/Rectangles.txt')
     do i = 1, NoCP
         read(3, *) Rect(i,:)
     end do
     close(3)
     
-    open(4, file='Input_Data/Mesh_coarse.txt')
+    open(4, file= InFolder//'/Mesh_coarse.txt')
     read(4, *) nbc
     allocate(connecc(nbc,NoDim+1))
     allocate(coarse(np-nbf, NoDim+2))
@@ -65,17 +66,17 @@ contains
     
     end subroutine SubReadData
     
-    subroutine InitSnapshots(filename, istr, coord_temp, boundff, NoDim)
+    subroutine InitSnapshots(filename, istr, coord_temp, boundff, NoDim, OutFolder)
     !Objective: Create Outputfile of each Snapshot as Input for the Pre Processor
     
         ! Variables
         real, dimension(np,NoDim) :: coord_temp
         integer, dimension(nbf,(NoDim+1)) :: boundff
-        character(len=*) :: filename
+        character(len=*) :: filename, OutFolder
         character(len=*) :: istr
     
         ! Body of InitSnapshots
-        open(99, file='Output_Data/'//filename//istr//'.dat')
+        open(99, file= OutFolder//'/'//filename//istr//'.dat')
         write(99,*) 1
         write(99,*) 'David Naumann'
         write(99,*) 'NoTrgElem NoNodes NoBound'        
@@ -97,41 +98,39 @@ contains
     
     end subroutine InitSnapshots
     
-    subroutine PreProInpFile(filename, istr)
+    subroutine PreProInpFile(filename, istr, InFolder, OutFolder)
     ! Objective: Create the Inputfile for the PreProcessor in Case of the Windows Executable
     
         ! Variables
-        character(len=*) :: filename
+        character(len=*) :: filename, InFolder, OutFolder
         character(len=*) :: istr
     
         ! Body of PreProInpFile
-        open(11, file='Input_Data/PreprocessingInput.txt')        
-        write(11,*) 'Output_Data/', filename, istr, '.dat'  ! Name of Input file
+        open(11, file=InFolder//'/PreprocessingInput.txt')        
+        write(11,*) OutFolder, '/', filename, istr, '.dat'  ! Name of Input file
         write(11,*) 'f'                                     ! Hybrid Mesh?
         write(11,*) 1                                       ! Number of Grids
         write(11,*) 0                                       ! Directionality Parameters
         write(11,*) 0                                       ! Visualization Modes
-        write(11,*) 'Output_Data/', filename, istr, '.sol'  ! Output File name
+        write(11,*) OutFolder, '/', filename, istr, '.sol'  ! Output File name
         close(11)
     
     end subroutine PreProInpFile
     
-    subroutine WriteSolverInpFile(filename, istr, engFMF, hMa, NoIter, newdir, NoNests)
+    subroutine WriteSolverInpFile(filename, istr, engFMF, hMa, NoIter, newdir, NoNests, defPath, InFolder, OutFolder, SystemType)
     ! Objective: Create Solver Input files
     
         ! Variables
-        character(len=*) :: filename
-        character(len=*) :: istr
+        character(len=*) :: filename, istr, defPath, InFolder, OutFolder, newdir, SystemType
         character(len=:), allocatable :: fname
         character(len=7) :: strEFMF, strMa      ! String for Ma number & Engine Inlet Front Mass Flow Number
         character(len=2) :: strNI               ! String for Number of Iterations
-        character(len=*) :: newdir              ! New Directory
         integer :: fileLength
     
         ! Body of WriteSolverInpFile
         fileLength = len(filename) + len(istr) + 15
         allocate(character(len=fileLength) :: fname)
-        fname = 'Input_Data/'//filename//istr//'.inp'
+        fname = InFolder//'/'//filename//istr//'.inp'
         write( strEFMF, '(F7.3)' )  engFMF
         write( strMa, '(F7.3)' )  hMa
         write( strNI, '(I2)' )  NoIter
@@ -178,43 +177,70 @@ contains
         close(5)
         
         ! Create Read File for Solver
-        open(1, file='Input_Data/SolverInput'//istr//'.txt')
-        write(1,*) '2DEngInlSim/', newdir, '/Input/', filename, istr, '.inp'    ! Control Filename
-        write(1,*) '2DEngInlSim/', newdir, '/Input/', filename, istr, '.sol'    ! Computation Filename
-        write(1,*) ''
-        write(1,*) '2DEngInlSim/', newdir, '/Output/', filename, istr, '.resp'  ! Result filename
-        write(1,*) '2DEngInlSim/', newdir, '/Output/', filename, istr, '.rsd'   ! Residual Filename
+        open(1, file=InFolder//'/SolverInput'//istr//'.txt')
+        if (SystemType == 'B') then
+            write(1,*) filename, istr, '.inp'    ! Control Filename
+            write(1,*) filename, istr, '.sol'    ! Computation Filename
+            write(1,*) ''
+            write(1,*) filename, istr, '.resp'  ! Result filename
+            write(1,*) filename, istr, '.rsd'   ! Residual Filename
+        else
+            write(1,*) '2DEngInlSim/', newdir, '/', InFolder, '/', filename, istr, '.inp'    ! Control Filename
+            write(1,*) '2DEngInlSim/', newdir, '/', InFolder, '/', filename, istr, '.sol'    ! Computation Filename
+            write(1,*) ''
+            write(1,*) '2DEngInlSim/', newdir, '/', OutFolder, '/', filename, istr, '.resp'  ! Result filename
+            write(1,*) '2DEngInlSim/', newdir, '/', OutFolder, '/', filename, istr, '.rsd'   ! Residual Filename
+        end if
         close(1)
     
     end subroutine WriteSolverInpFile
     
-    subroutine writeBatchFile(filename, istr, path, newdir)
+    subroutine writeBatchFile(filename, istr, path, newdir, SystemType, InFolder, OutFolder)
     
         ! Variables
-        character(len=*) :: filename, istr, path, newdir
+        character(len=*) :: filename, istr, path, newdir, SystemType, InFolder, OutFolder
         character(len=255) :: Output
     
         ! Body of writeBatchFile
-        open(1, file='Input_Data/batchfile'//istr//'.sh')   
-        write(1,*) '#PBS -N ' ,fileName, istr
-        write(1,*) '#PBS -q oh'
-        write(1,*) '#PBS -l nodes=1:ppn=1'
-        write(1,*) '#PBS -l walltime=24:00:00'
-        write(1,*) '#PBS -l mem=1gb'
-        write(1,*) '#PBS -m bea'
-        write(1,*) '#PBS -M 717761@swansea.ac.uk'
-        Output = path//' < /eng/cvcluster/egnaumann/2DEngInlSim/'//newdir//'/Input/SolverInput'//istr//'.txt'
-        write(1,'(A)') trim(Output)
+        open(1, file= InFolder//'/batchfile'//istr//'.sh')  
+        if (SystemType == 'Q') then
+            write(1,*) '#PBS -N ' ,fileName, istr
+            write(1,*) '#PBS -q oh'
+            write(1,*) '#PBS -l nodes=1:ppn=1'
+            write(1,*) '#PBS -l walltime=24:00:00'
+            write(1,*) '#PBS -l mem=1gb'
+            write(1,*) '#PBS -m bea'
+            write(1,*) '#PBS -M 717761@swansea.ac.uk'
+            Output = path//' < /eng/cvcluster/egnaumann/2DEngInlSim/'//newdir//'/'//InFolder//'/SolverInput'//istr//'.txt'
+            write(1,'(A)') trim(Output)
+        else
+            write(1,*) '#BSUB -J ' ,fileName, istr
+            write(1,*) '#BSUB -o ' ,fileName, istr, '.o'
+            write(1,*) '#BSUB -e ' ,fileName, istr, '.e'
+            !write(1,*) '#BSUB -q <enter a queue>'
+            write(1,*) '#BSUB -n 1'
+            write(1,*) '#BSUB -W 24:00'
+            write(1,*) 'ssh cf-log-001'
+            Output = path//' < /home/david.naumann/2DEngInlSim/'//newdir//'/'//InFolder//'/SolverInput'//istr//'.txt'
+            write(1,'(A)') trim(Output)
+            write(1,*) 'cd ..'
+            write(1,*) 'mv ', InFolder, '/', filename, istr, '.rsd ', OutFolder, '/', filename, istr, '.rsd'
+            write(1,*) 'mv ', InFolder, '/', filename, istr, '.resp ', OutFolder, '/', filename, istr, '.resp'
+            write(1,*) '#BSUB -n 1'
+            !#BSUB -R "span[ptile=12]"
+            
+        end if
+        close(1)
     
     end subroutine writeBatchFile
     
-    subroutine createDirectoriesInit(newdir)
+    subroutine createDirectoriesInit(newdir, defPath, InFolder, OutFolder)
     ! Objective: Create Directories from Windows in Linux
     
         ! Variables
         character(len=255) :: currentDir
         character(len=:), allocatable :: Output
-        character(len=*) :: newdir
+        character(len=*) :: newdir, defPath, InFolder, OutFolder
         integer :: strOut
         
         ! Body of createDirectories
@@ -222,22 +248,28 @@ contains
         open(1, file='FileCreateDir.scr')
         write(1,*) 'cd '
         write(1,*) 'cd ..'
-        write(1,*) 'cd egnaumann/2DEngInlSim'
+        write(1,*) 'cd ', defPath
+        write(1,*) 'cd ..'
+        write(1,*) 'chmod 777 2DEngInlSim'
+        write(1,*) 'cd 2DEngInlSim'
         write(1,*) 'mkdir ', newdir
         write(1,*) 'cd ', newdir
-        write(1,*) 'mkdir Input'
-        write(1,*) 'mkdir Output'
+        write(1,*) 'mkdir ', InFolder
+        write(1,*) 'mkdir ', OutFolder
+        write(1,*) 'cd ..'
+        write(1,*) 'cd ..'
+        write(1,*) 'chmod 711 2DEngInlSim'
         close(1)
     
     end subroutine createDirectoriesInit
     
-    subroutine transferFilesWin(filename, istr, newdir)
+    subroutine transferFilesWin(filename, istr, newdir, defPath, InFolder, OutFolder)
     ! Objective: Create Directories from Windows in Linux
     
         ! Variables
         character(len=255) :: currentDir
         character(len=:), allocatable :: Output
-        character(len=*) :: filename, istr, newdir
+        character(len=*) :: filename, istr, newdir, defPath, InFolder, OutFolder
         integer :: strOut
         
         ! Body of createDirectories
@@ -245,42 +277,42 @@ contains
         open(1, file='FileCreateDir.scr')
         write(1,*) 'cd '
         write(1,*) 'cd ..'
-        write(1,*) 'cd egnaumann/2DEngInlSim/', newdir, '/Input'
+        write(1,*) 'cd ', defPath , '/', newdir, '/', InFolder
         
         ! Put Commmands to Transfer Data from Input_Data Folder on Windows to created Input Folder on Cluster
         strOut = len(trim(currentDir)) + 33
         allocate(character(len=strOut) :: Output)
-        Output = 'put "'//trim(currentDir)//'/Input_Data/SolverInput'//istr//'.txt"'
+        Output = 'put "'//trim(currentDir)//'/'//InFolder//'/SolverInput'//istr//'.txt"'
         write(1, '(A)') Output       
         deallocate(Output)
                       
         strOut = len(trim(currentDir)) + 22
         allocate(character(len=strOut) :: Output)
-        Output = 'put "'//trim(currentDir)//'/Output_Data/'//filename//istr//'.sol"'
+        Output = 'put "'//trim(currentDir)//'/'//OutFolder//'/'//filename//istr//'.sol"'
         write(1, '(A)') Output
         deallocate(Output)
         
         strOut = len(trim(currentDir)) + 23
         allocate(character(len=strOut) :: Output)
-        Output = 'put "'//trim(currentDir)//'/Input_Data/'//filename//istr//'.inp"'
+        Output = 'put "'//trim(currentDir)//'/'//InFolder//'/'//filename//istr//'.inp"'
         write(1, '(A)') Output
         deallocate(Output)
         
         strOut = len(trim(currentDir)) + 25
         allocate(character(len=strOut) :: Output)
-        Output = 'put "'//trim(currentDir)//'/Input_Data/batchfile'//istr//'.sh"'
+        Output = 'put "'//trim(currentDir)//'/'//InFolder//'/batchfile'//istr//'.sh"'
         write(1, '(A)') Output
         close(1)
     
     end subroutine transferFilesWin
     
-    subroutine transferFilesLin(filename, istr, newdir)
-    ! Objective: Create Directories from Linux in Linux
+    subroutine transferFilesLin(filename, istr, newdir, defPath, InFolder, OutFolder)
+    ! Objective: Move files from Linux in Linux
     
         ! Variables
         character(len=255) :: currentDir
         character(len=255) :: Output
-        character(len=*) :: filename, istr, newdir 
+        character(len=*) :: filename, istr, newdir, defPath, InFolder, OutFolder 
         
         ! Body of createDirectories
         call getcwd(currentDir)
@@ -288,36 +320,41 @@ contains
         open(1, file='FileCreateDir.scr')
         write(1,*) 'cd '
         write(1,*) 'cd ..'
-        write(1,*) 'cd egnaumann/2DEngInlSim/', newdir, '/Input'
+        write(1,*) 'cd ', defPath, '/', newdir, '/', InFolder
 
-        Output = 'mv '//trim(currentDir)//'/Input_Data/SolverInput'//istr//'.txt SolverInput'//istr//'.txt'
+        Output = 'mv '//trim(currentDir)//'/'//InFolder//'/SolverInput'//istr//'.txt SolverInput'//istr//'.txt'
         write(1, '(A)') trim(Output)
         
-        Output = 'mv '//trim(currentDir)//'/Output_Data/'//filename//istr//'.sol '//filename//istr//'.sol'
+        Output = 'mv '//trim(currentDir)//'/'//OutFolder//'/'//filename//istr//'.sol '//filename//istr//'.sol'
         write(1, '(A)') trim(Output)
         
-        Output = 'mv '//trim(currentDir)//'/Input_Data/'//filename//istr//'.inp '//filename//istr//'.inp'
+        Output = 'mv '//trim(currentDir)//'/'//InFolder//'/'//filename//istr//'.inp '//filename//istr//'.inp'
         write(1, '(A)') trim(Output)
         
-        Output = 'mv '//trim(currentDir)//'/Input_Data/batchfile'//istr//'.sh batchfile'//istr//'.sh'
+        Output = 'mv '//trim(currentDir)//'/'//InFolder//'/batchfile'//istr//'.sh batchfile'//istr//'.sh'
         write(1, '(A)') trim(Output)
         
         close(1)
     
     end subroutine transferFilesLin
     
-    subroutine TriggerFile(filename, istr, newdir)
+    subroutine TriggerFile(filename, istr, newdir, defPath, InFolder)
     ! Objectives: Triggerfile for Cluster Simulation & Parallelisation
     
         ! Variables
-        character(len=*) :: filename, istr, newdir
+        character(len=*) :: filename, istr, newdir, defPath, InFolder
     
         ! Body of TriggerFile
         open(1, file='Trigger.sh')
         write(1,*) 'cd '
         write(1,*) 'cd ..'
-        write(1,*) 'cd egnaumann/2DEngInlSim/', newdir, '/Input'
-        write(1,*) 'qsub batchfile', istr, '.sh'
+        write(1,*) 'cd ', defPath, '/', newdir, '/', InFolder
+        
+        if (SystemType == 'Q') then
+            write(1,*) 'qsub batchfile', istr, '.sh'
+        else
+            write(1,*) 'bsub < batchfile', istr, '.sh'
+        end if
         close(1)
                         
     end subroutine TriggerFile
@@ -339,40 +376,45 @@ contains
                         
     end subroutine TriggerFile2
     
-    subroutine CheckSimStatus(newdir, filename, NoNests)
-    ! Objectives: Regularly checks for the last error file and stores response in check.txt file
+    subroutine CheckSimStatus(newdir, filename, istr, SystemType, defPath, InFolder)
+    ! Objectives: writes file that checks for error files and stores response in check.txt file
     
         ! Variables
-        character(len=*) :: newdir, filename
+        character(len=*) :: newdir, filename, istr, SystemType, defPath, InFolder
         character(len=255) :: Output
     
         ! Body of CheckSimStatus
         open(1, file='CheckStatus.scr')
         write(1,*) 'cd '
         write(1,*) 'cd ..'
-        write(1,*) 'cd egnaumann/2DEngInlSim/', newdir, '/Output'      
-        write(1,*) '[ -e ', filename, NoNests, '.e* ] && echo 1 > check.txt || echo 0 > check.txt'
-        if (IsLin == 'Y')   then
+        write(1,*) 'cd ', defPath, '/', newdir, '/', InFolder      
+        write(1,*) '[ -e ', filename, istr, '.e* ] && echo 1 > check.txt || echo 0 > check.txt'
+        if (SystemType == 'Q')   then
             write(1,*) 'cd ..'
             write(1,*) 'cd ..'
-            Output = 'mv egnaumann/2DEngInlSim/'//newdir//'/Output/check.txt check.txt'
+            Output = 'mv /eng/cvcluster/egnaumann/2DEngInlSim/'//newdir//'/'//InFolder//'/check.txt check.txt'
+            write(1, '(A)') trim(Output)
+        elseif (SystemType == 'B') then
+            write(1,*) 'cd ..'
+            write(1,*) 'cd ..'
+            Output = 'mv /home/david.naumann/2DEngInlSim/'//newdir//'/'//InFolder//'/check.txt check.txt'
             write(1, '(A)') trim(Output)
         end if
         close(1)
     
     end subroutine CheckSimStatus
     
-    subroutine CheckSimStatus2(newdir)
+    subroutine CheckSimStatus2(newdir, defPath, InFolder)
     ! Objectives: Transfers Check file to correct folder
     
         ! Variables
-        character(len=*) :: newdir
+        character(len=*) :: newdir, defPath, InFolder
     
         ! Body of CheckSimStatus
         open(1, file='CheckStatus.scr')
         write(1,*) 'cd '
         write(1,*) 'cd ..'       
-        write(1,*) 'cd egnaumann/2DEngInlSim/', newdir, '/Output'
+        write(1,*) 'cd ', defPath, '/', newdir, '/', InFolder
         write(1,*) 'get check.txt'       
         close(1)
     
