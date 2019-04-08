be4d75166df64777e214b091919bdb58551e7d28
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-04-08 22:20:44 +0100

    remove hacky fix for OV%engInNodes implemented by Mark

--- source/Optimization.f90
+++ source/Optimization.f90
@@ -1228,9 +1228,7 @@ module Optimization
             ! Old Bernoulli Equation to calculate non-dimensional pressure:  pressure(:,i) = e + (1.0/2.0)*(IV%Ma**2)*rho*(Vx*Vx + Vy*Vy) 
             
             ! Local Mach number non-dimensional
-            if(allocated(OV%engInNodes)) then
-                OV%MaLocal(:,(i - Start + 1)) = sqrt(Vx(OV%engInNodes)**2 + Vy(OV%engInNodes)**2)/(sqrt(IV%gamma*OV%pressure(OV%engInNodes,(i - Start + 1))/rho(OV%engInNodes)))
-            end if
+            OV%MaLocal(:,(i - Start + 1)) = sqrt(Vx(OV%engInNodes)**2 + Vy(OV%engInNodes)**2)/(sqrt(IV%gamma*OV%pressure(OV%engInNodes,(i - Start + 1))/rho(OV%engInNodes)))
             
             
 ! ambient pressure calculated based on Reynolds number and Ma and input Temperature            
