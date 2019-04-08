d4ed988b61e528ba76af4ef13abf5578bb90215c
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-04-08 21:14:16 +0100

    read until last line refactored

--- source/Optimization.f90
+++ source/Optimization.f90
@@ -2359,7 +2359,7 @@ module Optimization
     
         ! Variables
         implicit none
-        integer :: NoSnapshot, j, io
+        integer :: FileSize, LastLine, NoSnapshot, j
         double precision, dimension(8) :: Input      
         double precision :: Lift, Drag, Fi
        
@@ -2367,20 +2367,23 @@ module Optimization
         call DetermineStrLen(istr, NoSnapshot)
         open(11, file=newdir//'/'//OutFolder//'/'//trim(IV%filename)//istr//'.rsd', form='formatted',status='old')
         deallocate(istr)
-        
-        ! Read until last line
-        do
-            read(11, *, IOSTAT=io) Input
-            if(io > 0) then
-                STOP 'FILE INPUT ERROR'
-                exit
-            else if (io < 0) then
-                exit
+        inquire(11, size = FileSize)           
+        if (IV%NoDim == 3) then
+            LastLine = FileSize/175
+        else
+            if (IV%SystemType == 'W') then
+                LastLine = FileSize/106 !107
+            else     
+                LastLine = FileSize/106
             end if
+        end if
+            
+        ! Read until last line
+        do j = 1, (LastLine - 1)
+            read(11, *) Input
         end do
-        
+        read(11, *) Input
         close(11)
-        
         Lift = Input(3)
         Drag = Input(4)
         Fi = Lift/Drag
